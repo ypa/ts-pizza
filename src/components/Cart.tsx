05dd4e981ecd96d6eed73e88854402ff1f397851
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-01 16:02:12 -0800

    Toggle shopping cart item dropdown

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -20,11 +20,17 @@ class Cart extends React.Component<Props, State> {
   render() {
     return (
       <div className={CartCSS.cartContainer}>
-        <button className={CartCSS.button} type="button">
+        <button className={CartCSS.button} type="button" onClick={() => {
+          this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+        }} >
           <FiShoppingCart />
           <span>2 pizza(s)</span>
         </button>
-        <div className={CartCSS.cartDropDown}>
+        <div
+          className={CartCSS.cartDropDown} style={{
+            display: this.state.isOpen ? 'block': 'none',
+          }}
+        >
           <ul>
             <li>Napoletana</li>
             <li>Marinara</li>
