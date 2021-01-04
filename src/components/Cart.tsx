a4d24635b12c34ae32d3236e569e2771f227f36b
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-03 17:12:05 -0800

    Toggle the cart container adding DOM API event listener in component life cycle methods

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import CartCSS from './Cart.module.css';
 import { AppStateContext } from './AppState';
 
-interface Props {}
+interface Props { }
 
 interface State {
   isOpen: boolean;
 }
 
 class Cart extends React.Component<Props, State> {
+  #containerRef: React.RefObject<HTMLDivElement>;
 
   constructor(props: Props) {
     super(props);
     this.state = {
       isOpen: false,
     };
+
+    this.#containerRef = createRef();
   }
 
   handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
   };
+
+  handleOutsideClick = (event: MouseEvent) => {
+    if (
+      this.#containerRef.current &&
+      !this.#containerRef.current.contains(event.target as Node)
+    ) {
+      this.setState({ isOpen: false });
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleOutsideClick);
+  }
+
+  // Clean up event listeners to prevent memory leaks
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleOutsideClick);
+  }
 
   render() {
     return (
@@ -31,7 +52,7 @@ class Cart extends React.Component<Props, State> {
         }, 0);
 
         return (
-          <div className={CartCSS.cartContainer}>
+          <div className={CartCSS.cartContainer} ref={this.#containerRef}>
             <button className={CartCSS.button} type="button" onClick={this.handleClick} >
               <FiShoppingCart />
               <span>{itemsCount} pizza(s)</span>
