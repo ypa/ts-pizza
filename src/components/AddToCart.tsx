bc5d13dc690a7ac18803dc321587212d1d68a9e3
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-03 16:00:21 -0800

    Add Higher order component that simply wraps a child component and renders it

---
+++ src/components/AddToCart.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function withAddToCart<OriginalProps>(ChildComponent: React.ComponentType<OriginalProps>) {
+  const AddToCartHOC = (props: OriginalProps) => {
+    return <ChildComponent {...props} />;
+  };
+  return AddToCartHOC;
+}
